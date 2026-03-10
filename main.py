7f9fd775c98e76548fe5617392db092cbad87b35
swapnil-up <swapnilupadhyaykharel@gmail.com>
2026-03-10 13:11:53 +0545

    removed comments

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ async def shutdown():
     await database.disconnect()
 
 
-# Endpoint: Get attendance percentage for one user
 @app.get("/attendance/{emp_id}", response_model=AttendancePercentage)
 async def get_attendance_percentage(emp_id: int):
     query = "SELECT attendance_percentage(:emp_id) AS percentage;"
@@ -28,28 +27,24 @@ async def get_attendance_percentage(emp_id: int):
     return {"emp_id": emp_id, "percentage": result["percentage"]}
 
 
-# Endpoint: List all attendance records
 @app.get("/attendances", response_model=List[Attendance])
 async def list_attendances():
     query = "SELECT emp_id, is_present, date FROM attendances ORDER BY date, emp_id;"
     rows = await database.fetch_all(query=query)
     return rows
 
-# Endpoint: List all users
 @app.get("/users", response_model=List[Employee])
 async def list_employees():
     query = "SELECT * from employee"
     rows = await database.fetch_all(query=query)
     return rows
 
-# Endpoint: Retrieve specific users
 @app.get("/users/{id}", response_model=Employee)
 async def get_employee(id: int):
     query = "SELECT * from employee e where e.id=(:emp_id)"
     rows = await database.fetch_one(query=query, values={"emp_id": id})
     return rows
 
-# Endpoint: Change attendance status to true
 @app.patch("/attendance/{att_id}", response_model=Attendance)
 async def update_is_present_true(att_id: int):
     query = "update attendances a set is_present = true where a.id=:att_id RETURNING emp_id, is_present, date, updated_at"
@@ -58,7 +53,6 @@ async def update_is_present_true(att_id: int):
         raise HTTPException(status_code=404, detail="Attendance record not found")
     return rows
 
-# Endpoint: Show dashboard concept
 @app.get("/dashboard", response_model=List[EmployeeDashboard])
 async def list_employees_dashboard():
     query = "SELECT * from employee_dashboard"

03480e8098502380c7acb1fb73f11c55c2fe765a
swapnil-up <swapnilupadhyaykharel@gmail.com>
2026-03-10 11:45:50 +0545

    chore: added comments

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ import asyncio
 DATABASE_URL = "postgresql://postgres:password@localhost:5432/ksystem"
 database = Database(DATABASE_URL)
 
+# Function to listen to postgres notifications
 async def attendance_listener():
     conn = await asyncpg.connect(DATABASE_URL)
     async def callback(conn, pid, channel, payload):
